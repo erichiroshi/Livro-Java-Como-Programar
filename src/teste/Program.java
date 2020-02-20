9f21e30f1c3d59593c38ca566842cc0cd3413593
erichiroshi <erichiroshi@gmail.com>
2020-02-20 13:33:27 -0400

    cap 15

--- src/teste/Program.java
+++ src/teste/Program.java
@@ -1,48 +1,16 @@
 package teste;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.TreeSet;
-
 public class Program {
 
 	public static void main(String[] args) {
 
-		Scanner sc = new Scanner(System.in);
-
-		System.out.println("Dígite 5 números inteiros");
-
-		Set<Integer> lista = new TreeSet<>();
-
-		for (int i = 0; i < 5; i++) {
-			lista.add(sc.nextInt());
-
+		System.err.println("teste");
+		System.out.println("teste");
+		try {
+			throw new Exception("lançou erro");
+		} catch (Exception e) {
+			System.err.println("teste " + e.getMessage());
 		}
-
-		Integer[] array = lista.toArray(new Integer[0]);
-		
-		for(Integer a : array)
-		System.out.print(a + " ");
-		System.out.println();
-		System.out.println();
-
-		System.out.println(lista);
-		System.out.println("Menor: " + Collections.min(lista));
-		System.out.println("Maior: " + Collections.max(lista));
-
-		List<Integer> list = Arrays.asList(array);
-
-		Collections.reverse(list);
-		System.out.println(list);
-		
-		System.out.println();
-		for(Integer a : array)
-		System.out.print(a + " ");
-		sc.close();
-		
 		
 	}
 
